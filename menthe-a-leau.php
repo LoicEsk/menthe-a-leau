9532521f288929923de658554f8b38e170d7b75d
bloodyRaccoon <loic.laurent@outlook.com>
2015-06-10 12:29:26 +0200

    fix bug on shortcode

--- menthe-a-leau.php
+++ menthe-a-leau.php
@@ -80,9 +80,34 @@ function datalizer_enqueue($hook) {
 // shortcode intégration
 function shortcode_datalizer($attr){
 	datalizer_enqueue(null);
-	ob_start();
+	/*ob_start();
 	include(plugin_dir_path( __FILE__ ).'template/output-datalizer.php');
-	return ob_get_clean();
+	return ob_get_clean();*/
+
+	$output = '
+	<div id="datalizer">
+		<script type="text/javascript">
+			// config
+		</script>
+		<div class="loaderLayout">Chargement ...</div>
+		<canvas id="graph" width="100%"></canvas>
+		<form id="settings">
+			<select id="interval">
+			    <option value="535680">Un an</option>
+				<option value="267840">6 mois</option>
+				<option value="133920">3 mois</option>
+				<option value="44640">1 mois</option>
+				<option value="10080" selected="true">1 semaine</option>
+				<option value="1440">24 heures</option>
+			</select>
+			<input type="text" id="dateFin" value="NOW">
+			
+			<table id="dataSelect">
+			</table>
+		</form>
+		<div id="ajaxOut"></div>
+	</div>';
+	return $output;
 }
 add_shortcode( 'datalizer', 'shortcode_datalizer' );
 
